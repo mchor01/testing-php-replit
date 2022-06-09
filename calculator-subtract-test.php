240375b8d81c77f94cc7239087dbc0bb517c8e06
mchor01 <81789617+mchor01@users.noreply.github.com>
2022-06-09 16:40:11 +0000

    Added subtract function on Calculator.php

--- calculator-subtract-test.php
+++ calculator-subtract-test.php
@@ -6,7 +6,7 @@ include 'Calculator.php';
 class TestOfCalculator extends UnitTestCase {
 	function testSubtract() {
 	  $myCalc = new Calculator();
-	  $this->assertEqual($myCalc->subract(4,2), 2, "4 - 2");
+	  $this->assertEqual($myCalc->subtract(4,2), 2, "4 - 2");
 	  $this->assertEqual($myCalc->subtract(7,3), 4, "7 - 3");
 
 	}
